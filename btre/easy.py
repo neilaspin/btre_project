a90da8ad9d5b62353f430554665daba5dc64e8fc
neilaspin <neilaspin@clara.co.uk>
2020-05-28 13:03:38 +0100

    summar
    commits

---
+++ btre/easy.py
@@ -0,0 +1,6 @@
+movie_title = 'Star Wars'
+
+print(movie_title.lower())
+print(movie_title.upper())
+print(movie_title.count('a'))
+
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Realtor
+
+# admin 
+admin.site.register(Realtor)
